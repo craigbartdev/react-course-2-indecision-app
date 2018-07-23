9b136fff0d0c44d0fce3388ea1fb0c8e34cca5d8
Craig Bartholomew <craigbartjr@yahoo.com>
2018-07-23 16:40:13 -0400

    Setup devDependencies and prod server

--- src/app.js
+++ src/app.js
@@ -4,7 +4,5 @@ import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-console.log('testing');
-
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -51,7 +51,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map', 
         //cheap module source map fails for development after production source map is added
         devServer: {
-            contentBase: path.join(__dirname, 'public')
+            contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/'
         }
     }
 }
